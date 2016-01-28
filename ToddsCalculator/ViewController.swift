6232950c90e3837c91bdadda2cf88a8c009bc5d5
Liuyuan Tan <tanliuyuan@gmail.com>
2016-01-28 14:15:52 -0600

    Initial Commit

---
+++ ToddsCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToddsCalculator
+//
+//  Created by Liuyuan Tan on 1/28/16.
+//  Copyright © 2016 Liuyuan "Todd" Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
